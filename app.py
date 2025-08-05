7b51dda0db5a0f8725a55f35e47b03a85e21c504
dedede101010 <dan.aguiar01@gmail.com>
2025-08-05 15:49:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ def serve_image_and_log():
     ip = request.remote_addr
     time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     with open("log.txt", "a") as log_file:
-        log_file.write(f"{time} - {ip}\n")
+    log_file.write(f"{time} - {ip}\n")
+    print(f"[LOG] {time} - {ip}")
+
     return send_file("myfaith.jpg", mimetype='image/jpeg')
 
 if __name__ == "__main__":
