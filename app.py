fa6ae8b8e683bdac8f941faaec666828170faa08
dedede101010 <dan.aguiar01@gmail.com>
2025-08-05 15:07:17 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, send_file, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def serve_image_and_log():
+    ip = request.remote_addr
+    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open("log.txt", "a") as log_file:
+        log_file.write(f"{time} - {ip}\n")
+    return send_file("myfaith.jpg", mimetype='image/jpeg')
+
+if __name__ == "__main__":
+    app.run()
