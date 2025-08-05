1cc347a93c3c19462a712997db0073373b39a9c2
dedede101010 <dan.aguiar01@gmail.com>
2025-08-05 17:14:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-    # Get IP from proxy header, fallback to remote_addr
     ip = request.headers.get('X-Forwarded-For', request.remote_addr)
 
-    with open("/tmp/ip_log.txt", "a") as log_file:
+    with open("ip_log.txt", "a") as log_file:
         log_file.write(f"{time} - {ip}\n")
 
     return "IP logged."
